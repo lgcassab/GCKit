59822d0b1b1796500bfe6d0f77906d0878ec341e
Gustavo Cassab <lgcassab@mac.com>
2013-10-20 11:46:28 -0300

    Adjusting tests

--- GCKitTests/NSStringTests.m
+++ GCKitTests/NSStringTests.m
@@ -18,9 +18,9 @@
 - (void)testHeightForText {
 	NSString *text = @"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 	CGFloat nHeight = [NSString heightForText:text withFont:[UIFont fontWithName:@"ArialMT" size:17.0f] constrainedToMaxWidth:300.0f];
-	NSUInteger uHeight = fabs(nHeight);
-	if (uHeight != 246) {
-		XCTFail(@"text height should be 246");
+	NSInteger uHeight = fabs(nHeight);
+	if (uHeight != 259) {
+		XCTFail(@"text height should be 259, but received %ld instead.", (long)uHeight);
 	}
 }
 
@@ -46,9 +46,9 @@
 - (void)testHeightWithFont {
 	NSString *text = @"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 	CGFloat nHeight = [text heightWithFont:[UIFont fontWithName:@"ArialMT" size:17.0f] constrainedToMaxWidth:300.0f];
-	NSUInteger uHeight = fabs(nHeight);
-	if (uHeight != 246) {
-		XCTFail(@"text height should be 246");
+	NSInteger uHeight = fabs(nHeight);
+	if (uHeight != 259) {
+		XCTFail(@"text height should be 259, but received %ld instead.", (long)uHeight);
 	}
 }
 
